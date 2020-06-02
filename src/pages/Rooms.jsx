ebf4edbe1039917c344e916bfafcf645c9f4b136
Altaan <ahmed.altaan90@gmail.com>
2020-06-02 10:52:36 +0200

    Updated the README file

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -5,6 +5,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import RoomsContainer from "../components/RoomsContainer";
 
+// This page renders the rooms according to the filtering parameters displayed in this page
 const Rooms = () => {
   return (
     <>
